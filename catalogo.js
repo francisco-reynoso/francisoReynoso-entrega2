85819a77106e0360efa5cc81d44730e959cf754d
francisco-reynoso <panchoreynoso251@gmail.com>
2023-09-18 15:04:33 -0300

    mejore la lista

--- catalogo.js
+++ catalogo.js
@@ -1,6 +1,7 @@
 alert("Bienvenidos a mi perfumeria");
 let continuar = prompt("Desea comprar algo").toLocaleLowerCase();
 const carrito = [];
+let precio = 0;
 
 
 function confirmacion(continuar) {
@@ -18,20 +19,20 @@ function verificacion(continuar) {
     else if (continuar == "no"){
         alert("gracias por su visita");
         mostrarProductos("esto son los productos en su carrito ",carrito);
+        alert(`y el total es: $${precio}`);
         
     }
 }
 
 function comprar(lista, carrito) {
-    let precio = 0;
     let pedido = prompt("ingrese lo que quiera comprar");
     let mensaje = ("el perfume no fue encontrado");
     for (const producto of lista) {
         if(pedido == producto.nombre) {
-            carrito.shift(producto.nombre);
+            carrito.push(new Producto(producto.nombre, producto.marca, producto.precio));
             mensaje = ("el perfume fue añadido al carrito");
             precio += producto.precio;
-            alert(`se añadio ${producto.nombre} y el total es ${precio}`);
+            alert(`se añadio ${producto.nombre} y su precio es ${producto.precio}`);
             continuar = prompt("¿desea comprar algo mas?");
             verificacion(continuar);
         }
