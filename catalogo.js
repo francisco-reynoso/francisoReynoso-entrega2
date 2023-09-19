b7db6a95d9fae8a973f424dfe25488ab31e3bfa3
francisco-reynoso <panchoreynoso251@gmail.com>
2023-09-18 23:17:21 -0300

    funciona mi carrito

--- catalogo.js
+++ catalogo.js
@@ -1,7 +1,8 @@
 alert("Bienvenidos a mi perfumeria");
 let continuar = prompt("Desea comprar algo").toLocaleLowerCase();
 const carrito = [];
-let precio = 0;
+
+
 
 
 function confirmacion(continuar) {
@@ -14,35 +15,47 @@ function verificacion(continuar) {
     }
     if (continuar == "si") {
         mostrarProductos("estos son los productos disponibles",lista);
-        comprar(lista,carrito);
+        comprar(lista);
     }
     else if (continuar == "no"){
         alert("gracias por su visita");
-        mostrarProductos("esto son los productos en su carrito ",carrito);
-        alert(`y el total es: $${precio}`);
-       
+        mostrarProductos("esto son los productos en su carrito ",carrito); 
         
     }
 }
 
-function comprar(lista, carrito) {
-    let pedido = prompt("ingrese lo que quiera comprar");
+function comprar(lista) {
+    let pedido = prompt("ingrese lo que quiera comprar").toLocaleLowerCase();
     let mensaje = ("el perfume no fue encontrado");
-    for (const producto of lista) {
-        if(pedido == producto.nombre) {
-            carrito.push(new Producto(producto.nombre, producto.marca, producto.precio));
-            mensaje = ("el perfume fue añadido al carrito");
-            precio += producto.precio;
-            alert(`se añadio ${producto.nombre} y su precio es ${producto.precio}`);
-            continuar = prompt("¿desea comprar algo mas?");
-            verificacion(continuar);
-        }
+    const result = lista.find((ele) => ele.nombre == pedido);
+    if (result != undefined  ) {
+        carrito.push(new Producto(result.nombre, result.marca, result.precio));
+        total = lista.reduce((acumulador) => acumulador + result.precio, 0);
+        alert("el perfume fue añadido al carrito");
+        continuar = prompt("¿desea comprar algo mas?");
+        verificacion(continuar);
     }
-    verificacion(continuar);
+    else{
+        alert ( mensaje);
+        continuar = prompt("¿desea intentar con otro perfume?");
+        verificacion(continuar);
+    }
 }
 confirmacion(continuar);
 
 
+
+// for (const producto of lista) {
+//     if(pedido == producto.nombre) {
+//         carrito.push(new Producto(producto.nombre, producto.marca, producto.precio));
+//         mensaje = ("el perfume fue añadido al carrito");
+//         precio += producto.precio;
+//         alert(`se añadio ${producto.nombre} y su precio es ${producto.precio}`);
+//         continuar = prompt("¿desea comprar algo mas?");
+//         verificacion(continuar);
+//     }
+// }
+// verificacion(continuar);
 
 
     
